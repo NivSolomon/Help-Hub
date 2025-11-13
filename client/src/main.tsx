3adaa686f04f7f1f247be60ba5545fe942afff4e
Niv Solomon <nivsolomon3@gmail.com>
2025-11-13 13:16:34 +0200

    update some files for compitability wirh Render

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import "./global.css";
 import { NotificationProvider } from "./components/NotificationCenter";
 import beaverFavicon from "./assets/BeaverHead.png";
 
